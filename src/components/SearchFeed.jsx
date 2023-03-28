0313847acf41c29798caf7c83d11f19e2de1cc4f
Gautam <pahwagautam786@gmail.com>
2023-03-28 18:10:32 +0530

    SearchFeed  created

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-
+import {useState,useEffect,}  from 'react'
+import {Box,Stack,Typography} from '@mui/material'
+import {Videos} from './'
+import {fetchFromAPI} from '../utils/fetchFromAPI'
+import { useParams } from 'react-router-dom'
 const SearchFeed = () => {
+
+  const [videos,setVideos]=useState(null);
+  const {searchTerm}=useParams();
+  useEffect(()=>{
+    // setVideos(null);
+       fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data)=> setVideos(data.items));
+       
+        //To get data  we add then to perfom some task when async function fetchFromAPI return its promise (.then is procedure for 
+        // async function .But for sync function we use 'const data=fetchFromAPI()'
+        
+       
+  },[searchTerm]);
+
   return (
-    <div>
-      SearchFeed
-    </div>
+
+      <Box
+      p={2} sw={{overflowY:'auto',
+      height:'90vh',flex:2
+    }}
+      >
+        <Typography
+        variant="h4" fontWeight="bold" mb={2} sx={{
+          color:'white'
+        }}>
+        Search Results for:
+          <span style={{color:'#F31503'}}> {searchTerm}</span> videos
+        </Typography>
+        
+        <Videos videos={videos}/>
+      </Box>
+
   )
 }
 
