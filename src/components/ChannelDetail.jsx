7f379d7034f7bf84db7b964f707109bbef81a282
Gautam <pahwagautam786@gmail.com>
2023-03-28 17:09:14 +0530

    channelDetail added

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -24,21 +24,21 @@ const ChannelDetail = () => {
     fetchResults();
   }, [id]);
   return (
-    // <Box minHeight='95vh'>
-    //   <Box>
-    //    <div style={{background: 'linear-gradient(90deg, rgba(216,179,222,1) 0%, rgba(40,141,203,1) 5%, rgba(187,108,195,1) 44%, rgba(188,141,190,1) 62%, rgba(31,205,252,1) 95%, rgba(218,181,219,1) 100%, rgba(0,212,255,1) 100%)'
-    //    ,zIndex:10,height:'300px'}}    
-    //    />
-    //    <ChannelCard channelDetail={channelDetail} marginTop='-110px'> </ChannelCard>
-    //   </Box>
-    //   <Box display='flex' p='2'>
-    //     <Box sx={{mr:{sm:'100px'} }}></Box>
-    //       <Videos videos={videos}></Videos>
+    <Box minHeight='95vh'>
+      <Box>
+       <div style={{background: 'linear-gradient(90deg, rgba(216,179,222,1) 0%, rgba(40,141,203,1) 5%, rgba(187,108,195,1) 44%, rgba(188,141,190,1) 62%, rgba(31,205,252,1) 95%, rgba(218,181,219,1) 100%, rgba(0,212,255,1) 100%)'
+       ,zIndex:10,height:'300px'}}    
+       />
+       <ChannelCard channelDetail={channelDetail} marginTop='-110px'> </ChannelCard>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{mr:{sm:'100px'} }}></Box>
+          <Videos videos={videos}></Videos>
 
         
-    //   </Box>
+      </Box>
 
-    // </Box>  
+    </Box>  
   )
 }
 
